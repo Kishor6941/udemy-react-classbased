5413b8498cac2b1d7f55d107f268c50ac07a0505
Kishor6941 <phawadeks9049@gmail.com>
2020-08-28 10:33:22 +0530

    git basic

--- src/person/Person.js
+++ src/person/Person.js
@@ -1,20 +1,34 @@
 import React from "react";
 import "./Person.css";
+import styled from "styled-components";
+
+const StyleDiv = styled.div`
+  width: 60%;
+  margin: 16px auto;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 16px;
+
+  @media (min-width: 500px) ": { width: "450px;
+`;
 
 const person = (props) => {
-  const style = {
+  /*  const style = {
     "@media (min-width:500px)": {
       width: "450px",
     },
-  };
+  }; */
   return (
-    <div className="Person" style={style}>
+    /*     <div className="Person" style={style}>
+     */
+
+    <StyleDiv>
       <p onClick={props.click}>
         I am {props.name} and I am {props.age} year old
       </p>
       <input type="text" onChange={props.changed} />
       <h1>{props.children}</h1>
-    </div>
+    </StyleDiv>
   );
 };
 
